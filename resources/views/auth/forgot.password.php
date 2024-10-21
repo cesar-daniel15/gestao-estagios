a26be9f40cfc358fefea229bd10eef3fd6094910
RubenBenedito <162583130+RubenBenedito@users.noreply.github.com>
2024-10-21 17:40:53 +0100

    Create forgot.password.php

---
+++ resources/views/auth/forgot.password.php
@@ -0,0 +1,10 @@
+@extends('layouts.default-page', ['showNavbar' => false, 'showFooter' => false])
+
+@section('title', 'Gestão de Estágios | Recuperar Password')
+
+@section('content')
+    <div class="flex justify-center items-center min-h-screen bg-gray-100">
+        <div class="flex w-full max-w-3xl bg-white rounded-xl shadow-lg mx-4">
+            <!-- Left Side -->
+
+
