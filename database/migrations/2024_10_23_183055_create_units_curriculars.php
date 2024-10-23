d834e27c4170815fbf7f214d2af74d5727fd3cab
RubenBenedito <162583130+RubenBenedito@users.noreply.github.com>
2024-10-23 19:34:09 +0100

    create_tables

---
+++ database/migrations/2024_10_23_183055_create_units_curriculars.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('units_curriculars', function (Blueprint $table) {
+            $table->id(); // Chave primária
+            $table->foreignId('institution_id')->constrained(); // Chave estrangeira para 'institutions'
+            $table->foreignId('course_id')->constrained(); // Chave estrangeira para 'courses'
+            $table->string('name'); // Nome da unidade curricular
+            $table->string('acronym'); // Acrônimo da unidade curricular
+            $table->timestamps(); // Gera created_at e updated_at automaticamente
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('units_curriculars');
+    }
+};
