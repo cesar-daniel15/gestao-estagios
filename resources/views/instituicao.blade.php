c91280e577a677d2a6641117c58b744235e962d7
RubenBenedito <162583130+RubenBenedito@users.noreply.github.com>
2024-10-29 18:25:36 +0000

    update pormenores

--- resources/views/instituicao.blade.php
+++ resources/views/instituicao.blade.php
@@ -4,6 +4,9 @@
 
 @section('navbar-items')
     <li class="mb-5 mt-2 md:mb-0 md:mt-0">
+        <a href="{{ url('/instituicao') }}" class="text-white text-gray-500 md:text-sky-400 text-base font-bold">Instituição</a>
+    </li>
+    <li class="mb-5 mt-2 md:mb-0 md:mt-0">
         <a href="{{ url('/empresa') }}" class="text-white md:text-gray-500 text-sm sm:text-base font-bold">Empresa</a>
     </li>
     <li class="mb-5 mt-5 md:mb-0 md:mt-0">
@@ -13,16 +16,26 @@
         <a href="{{ url('/aluno') }}" class="text-white md:text-gray-500 text-sm sm:text-base font-bold">Aluno</a>
     </li>
     <li class="inline-block transition-transform transform hover:scale-105 duration-300 ease-in-out mb-5 mt-5 md:mb-0 md:mt-0">
-        <a href="{{ url('/login') }}" class="bg-white md:bg-sky-400 text-sm sm:text-base font-bold md:text-white text-sky-400 p-2 rounded-xl px-6 sm:px-10">Começar Agora</a>
+        <a href="{{ url('/login') }}" class="bg-white md:bg-sky-400 text-sm sm:text-base font-bold md:text-white text-sky-400 p-2 rounded-xl px-6 sm:px-6">Começar Agora</a>
     </li>
 @endsection
 
 @section('content')
+        <!-- Seção Sobre -->
+        <section id="sobre">
+        <div class="w-full h-auto p-10 bg-gradient-to-r from-sky-400 to-blue-600 flex flex-col md:flex-row justify-center items-center text-center md:text-left">
 
-    <!-- Seção Sobre -->
-    <section id="sobre" class="p-6 md:p-10 bg-gradient-to-r from-sky-400 to-blue-600 flex flex-col justify-center items-center text-center">
-        <div class="text-xl sm:text-2xl md:text-3xl font-extrabold text-white mb-3 sm:mb-4 md:mb-5">Responsável da Instituição</div>
-        <p class="text-white max-w-lg sm:max-w-2xl text-sm sm:text-base md:text-lg text-justify leading-relaxed px-4">
+        <!-- Imagem do Coordenador (Centralizado Verticalmente) -->
+        <div class="w-full md:w-1/2 flex justify-center items-center mt-6 md:mt-0">
+            <svg xmlns="http://www.w3.org/2000/svg" width="7em" height="7em" viewBox="0 0 48 48">
+                <g fill="none" stroke="white" stroke-linejoin="round" stroke-width="4"><path d="M2 17.4L23.022 9l21.022 8.4l-21.022 8.4z"/><path stroke-linecap="round" d="M44.044 17.51v9.223m-32.488-4.908v12.442S16.366 39 23.022 39c6.657 0 11.467-4.733 11.467-4.733V21.825"/></g>
+            </svg>       
+        </div>
+
+    <!-- Texto de Apresentação (Alinhado à Esquerda) -->
+    <div class="w-full md:w-1/2 max-w-2xl md:text-left md:mr-8">
+        <div class="text-2xl md:text-3xl font-extrabold text-white mb-4 text-center">Responsável da Instituição</div>
+            <p class="text-white text-justify">
             A nossa plataforma simplifica a gestão de estagiários, permitindo acompanhar o progresso, aprovar planos e avaliar desempenhos de forma rápida e organizada. Com uma interface intuitiva, facilita a comunicação com responsáveis acadêmicos e centraliza toda a documentação necessária.
         </p>
     </section>
