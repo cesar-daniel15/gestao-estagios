576b647e5e4c5083cf3c462f44b4cb1fcad5a5a3
RubenBenedito <162583130+RubenBenedito@users.noreply.github.com>
2024-10-29 15:44:40 +0000

    update views

--- resources/views/aluno.blade.php
+++ resources/views/aluno.blade.php
@@ -3,6 +3,9 @@
 @section('title', 'Aluno - Gestão de Estágios')
 
 @section('navbar-items')
+    <li class="mb-5 mt-5 md:mb-0 md:mt-0">
+        <a href="{{ url('/instituicao') }}" class="text-white md:text-gray-500 text-base font-bold">Instituição</a>
+    </li>
     <li class="mb-5 mt-2 md:mb-0 md:mt-0">
         <a href="{{ url('/empresa') }}" class="text-white md:text-gray-500 text-base font-bold">Empresa</a>
     </li>
@@ -10,10 +13,10 @@
         <a href="{{ url('/coordenadores') }}" class="text-white md:text-gray-500 text-base font-bold">Coordenadores</a>
     </li>
     <li class="mb-5 mt-5 md:mb-0 md:mt-0">
-        <a href="{{ url('/instituicao') }}" class="text-white md:text-gray-500 text-base font-bold">Instituição</a>
+        <a href="{{ url('/aluno') }}" class="text-white text-gray-500 md:text-sky-400 text-base font-bold">Aluno</a>
     </li>
     <li class="inline-block transition-transform transform hover:scale-105 duration-300 ease-in-out mb-5 mt-5 md:mb-0 md:mt-0">
-        <a href="{{ url('/login') }}" class="bg-white md:bg-sky-400 text-base font-bold md:text-white text-sky-400 p-2 rounded-xl px-10">Começar Agora</a>
+        <a href="{{ url('/login') }}" class="bg-white md:bg-sky-400 text-base font-bold md:text-white text-sky-400 p-2 rounded-xl px-6">Começar Agora</a>
     </li>
 @endsection
 
@@ -21,9 +24,19 @@
 @section('content')
     <!-- Seção Sobre -->
     <section id="sobre">
-        <div class="w-full h-auto p-10 bg-gradient-to-r from-sky-400 to-blue-600 flex flex-col justify-center items-center text-center">
-            <div class="text-2xl md:text-3xl font-extrabold text-white mb-4 my-5">Aluno</div>
-            <p class="text-white max-w-2xl text-justify">
+        <div class="w-full h-auto p-10 bg-gradient-to-r from-sky-400 to-blue-600 flex flex-col md:flex-row justify-center items-center text-center md:text-left">
+
+        <!-- Imagem do Coordenador (Centralizado Verticalmente) -->
+        <div class="w-full md:w-1/2 flex justify-center items-center mt-6 md:mt-0">
+            <svg xmlns="http://www.w3.org/2000/svg" width="7em" height="7em" viewBox="0 0 256 256">
+                <path fill="white" d="m226.53 56.41l-96-32a8 8 0 0 0-5.06 0l-96 32A8 8 0 0 0 24 64v80a8 8 0 0 0 16 0V75.1l33.59 11.19a64 64 0 0 0 20.65 88.05c-18 7.06-33.56 19.83-44.94 37.29a8 8 0 1 0 13.4 8.74C77.77 197.25 101.57 184 128 184s50.23 13.25 65.3 36.37a8 8 0 0 0 13.4-8.74c-11.38-17.46-27-30.23-44.94-37.29a64 64 0 0 0 20.65-88l44.12-14.7a8 8 0 0 0 0-15.18ZM176 120a48 48 0 1 1-86.65-28.45l36.12 12a8 8 0 0 0 5.06 0l36.12-12A47.9 47.9 0 0 1 176 120m-48-32.43L57.3 64L128 40.43L198.7 64Z"/>
+            </svg>      
+        </div>
+    
+    <!-- Texto de Apresentação (Alinhado à Esquerda) -->
+    <div class="w-full md:w-1/2 max-w-2xl md:text-left md:mr-8">
+        <div class="text-2xl md:text-3xl font-extrabold text-white mb-4 text-center">Aluno</div>
+            <p class="text-white text-justify">
                 Esta plataforma auxilia o aluno na gestão do estágio, permitindo registrar atividades diárias, acompanhar a carga horária, e submeter documentos de forma rápida e organizada. Com uma interface intuitiva, facilita a comunicação com a empresa e com os orientadores acadêmicos, centralizando toda a documentação e registros necessários.
             </p>
         </div>
@@ -39,7 +52,9 @@
                 <div class="flex items-center justify-center mb-8 mt-8">
                     <!-- Ícone -->
                     <div class="flex-shrink-0">
-                        <img src="{{ asset('images/icons/pc-mobile.png') }}" alt="Ícone" class="w-12 h-12"> 
+                        <svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 24 24">
+                            <path fill="#00b0eb" d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.587 1.413T19 21zm0-4.05V19h14v-8.75L13.05 17L9 12.95zm0-2.85l4-4l3.95 3.95L19 7.25V5H5zm0-3.85v-3v6.8v-3.95v6.85v-4V17zm0 3.85V5v9.05v-3.95zm0 2.85v-4V17v-6.75V19z"/>
+                        </svg>                    
                     </div>
                     
                     <!-- Texto -->
@@ -54,7 +69,9 @@
                 <div class="flex items-center justify-center mb-8">
                     <!-- Ícone -->
                     <div class="flex-shrink-0">
-                        <img src="{{ asset('images/icons/pc-mobile.png') }}" alt="Ícone" class="w-12 h-12"> 
+                        <svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 24 24">
+                            <path fill="#00b0eb" d="M20.71 7.04c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.37-.39-1.02-.39-1.41 0l-1.84 1.83l3.75 3.75M3 17.25V21h3.75L17.81 9.93l-3.75-3.75z"/>
+                        </svg>                    
                     </div>
                     
                     <!-- Texto -->
@@ -68,7 +85,9 @@
                 <div class="flex items-center justify-center mb-8">
                     <!-- Ícone -->
                     <div class="flex-shrink-0">
-                        <img src="{{ asset('images/icons/ficheiro.png') }}" alt="Ícone" class="w-12 h-12"> 
+                        <svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 48 48">
+                            <path fill="#00b0eb" fill-rule="evenodd" stroke="#00b0eb" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="m4 24l5-5l10 10L39 9l5 5l-25 25z" clip-rule="evenodd"/>
+                        </svg>                   
                     </div>
                     
                     <!-- Texto -->
@@ -83,7 +102,9 @@
                     <div class="flex items-center justify-center mb-8">
                         <!-- Ícone -->
                         <div class="flex-shrink-0">
-                            <img src="{{ asset('images/icons/pc-mobile.png') }}" alt="Ícone" class="w-12 h-12"> 
+                            <svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 16 16">
+                                <path fill="none" stroke="#00b0eb" stroke-linejoin="round" d="M7.563 1.545H2.5v10.91h9V5.364M7.563 1.545L11.5 5.364M7.563 1.545v3.819H11.5m-7 9.136h9v-7M4 7.5h6M4 5h2m-2 5h6"/>
+                            </svg>                        
                         </div>
                         
                         <!-- Texto -->
@@ -106,7 +127,7 @@
                 <!-- Passo 2 -->
                 <div class="flex items-center space-x-3">
                     <div class="flex justify-center items-center bg-sky-400 text-white font-bold w-10 h-10 rounded-full text-center">1</div>
-                    <div class="bg-sky-400 text-white font-bold px-3 py-2 rounded-lg w-full text-left">Acedar ao Sistema</div>
+                    <div class="bg-sky-400 text-white font-bold px-3 py-2 rounded-lg w-full text-left">Aceder ao Sistema</div>
                 </div>
 
                 <!-- Passo 3 -->
@@ -115,21 +136,28 @@
                     <div class="bg-sky-400 text-white font-bold px-3 py-2 rounded-lg w-full text-left">Visualizar Ofertas de Estágio</div>
                 </div>
 
+                
                 <!-- Passo 4 -->
                 <div class="flex items-center space-x-3">
                     <div class="flex justify-center items-center bg-sky-400 text-white font-bold w-10 h-10 rounded-full text-center">3</div>
+                    <div class="bg-sky-400 text-white font-bold px-3 py-2 rounded-lg w-full text-left">Candidatar a Oferta de Estágio</div>
+                </div>
+
+                <!-- Passo 4 -->
+                <div class="flex items-center space-x-3">
+                    <div class="flex justify-center items-center bg-sky-400 text-white font-bold w-10 h-10 rounded-full text-center">4</div>
                     <div class="bg-sky-400 text-white font-bold px-3 py-2 rounded-lg w-full text-left">Registar Atividades do Dia a Dia</div>
                 </div>
 
                 <!-- Passo 5 -->
                 <div class="flex items-center space-x-3">
-                    <div class="flex justify-center items-center bg-sky-400 text-white font-bold w-10 h-10 rounded-full text-center">4</div>
+                    <div class="flex justify-center items-center bg-sky-400 text-white font-bold w-10 h-10 rounded-full text-center">5</div>
                     <div class="bg-sky-400 text-white font-bold px-3 py-2 rounded-lg w-full text-left">Registar Assiduidade</div>
                 </div>
 
                 <!-- Passo 6 -->
                 <div class="flex items-center space-x-3">
-                    <div class="flex justify-center items-center bg-sky-400 text-white font-bold w-10 h-10 rounded-full text-center">5</div>
+                    <div class="flex justify-center items-center bg-sky-400 text-white font-bold w-10 h-10 rounded-full text-center">6</div>
                     <div class="bg-sky-400 text-white font-bold px-3 py-2 rounded-lg w-full text-left">Submeter Relatório Final</div>
                 </div>
             </div>
--- resources/views/coordenadores.blade.php
+++ resources/views/coordenadores.blade.php
@@ -3,32 +3,49 @@
 @section('title', 'Coordenadores - Gestão de Estágios')
 
 @section('navbar-items')
+<li class="mb-5 mt-5 md:mb-0 md:mt-0">
+        <a href="{{ url('/instituicao') }}" class="text-white md:text-gray-500 text-base font-bold">Instituição</a>
+    </li>
     <li class="mb-5 mt-2 md:mb-0 md:mt-0">
         <a href="{{ url('/empresa') }}" class="text-white md:text-gray-500 text-base font-bold">Empresa</a>
     </li>
     <li class="mb-5 mt-5 md:mb-0 md:mt-0">
-        <a href="{{ url('/instituicao') }}" class="text-white md:text-gray-500 text-base font-bold">Instituição</a>
+        <a href="{{ url('/coordenadores') }}" class="text-white text-gray-500 md:text-sky-400 text-base font-bold">Coordenadores</a>
     </li>
     <li class="mb-5 mt-5 md:mb-0 md:mt-0">
         <a href="{{ url('/aluno') }}" class="text-white md:text-gray-500 text-base font-bold">Aluno</a>
     </li>
     <li class="inline-block transition-transform transform hover:scale-105 duration-300 ease-in-out mb-5 mt-5 md:mb-0 md:mt-0">
-        <a href="{{ url('/login') }}" class="bg-white md:bg-sky-400 text-base font-bold md:text-white text-sky-400 p-2 rounded-xl px-10">Começar Agora</a>
+        <a href="{{ url('/login') }}" class="bg-white md:bg-sky-400 text-base font-bold md:text-white text-sky-400 p-2 rounded-xl px-6">Começar Agora</a>
     </li>
 @endsection
 
 
 @section('content')
 
-    <!-- Seção Sobre -->
-    <section id="sobre">
-        <div class="w-full h-auto p-10 bg-gradient-to-r from-sky-400 to-blue-600 flex flex-col justify-center items-center text-center">
-            <div class="text-2xl md:text-3xl font-extrabold text-white mb-4 my-5">Responsável da Unidade Curricular</div>
-            <p class="text-white max-w-2xl text-justify">
-                O uso desta plataforma simplifica a gestão dos coordenadores, dando aos alunos a hipótese de estagiar, realizando várias tarefas,mas sempre de forma organizada. Com uma interface intuitiva, facilita a comunicação com responsáveis acadêmicos e centraliza toda a documentação necessária.
+<!-- Seção Sobre -->
+<section id="sobre">
+    <div class="w-full h-auto p-10 bg-gradient-to-r from-sky-400 to-blue-600 flex flex-col md:flex-row justify-center items-center text-center md:text-left">
+
+        <!-- Imagem do Coordenador (Centralizado Verticalmente) -->
+        <div class="w-full md:w-1/2 flex justify-center items-center mt-6 md:mt-0">
+            <svg xmlns="http://www.w3.org/2000/svg" width="7em" height="7em" viewBox="0 0 24 24">
+                <path fill="white" d="M20 17a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H9.46c.35.61.54 1.3.54 2h10v11h-9v2m4-10v2H9v13H7v-6H5v6H3v-8H1.5V9a2 2 0 0 1 2-2zM8 4a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2a2 2 0 0 1 2 2"/>
+            </svg>        
+        </div>
+
+        <!-- Texto de Apresentação (Alinhado à Esquerda) -->
+        <div class="w-full md:w-1/2 max-w-2xl md:text-left md:mr-8">
+            <div class="text-2xl md:text-3xl font-extrabold text-white mb-4">Responsável da Unidade Curricular</div>
+            <p class="text-white text-justify">
+                O uso desta plataforma simplifica a gestão dos coordenadores, dando aos alunos a hipótese de estagiar, realizando várias tarefas de forma organizada. Com uma interface intuitiva, facilita a comunicação com responsáveis acadêmicos e centraliza toda a documentação necessária.
             </p>
-        </div> 
-    </section>
+        </div>
+        
+    </div>
+</section>
+
+
 
     <!-- Seção Principais Funcionalidades -->
     <section id="funcionalidades">
@@ -39,7 +56,9 @@
                 <div class="flex items-center justify-center mb-8 mt-8">
                     <!-- Ícone -->
                     <div class="flex-shrink-0">
-                        <img src="{{ asset('images/icons/pc-mobile.png') }}" alt="Ícone" class="w-12 h-12"> 
+                        <svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 24 24">
+                            <path fill="#00b0eb" fill-rule="evenodd" d="M7 7c0-2.762 2.238-5 5-5s5 2.238 5 5v3h.4c.88 0 1.6.72 1.6 1.6v7c0 1.32-1.08 2.4-2.4 2.4H7.4C6.08 21 5 19.92 5 18.6v-7c0-.88.72-1.6 1.6-1.6H7zm8 0v3H9V7c0-1.658 1.342-3 3-3s3 1.342 3 3m-3 5.25a1.75 1.75 0 0 0-.75 3.332V18a.75.75 0 0 0 1.5 0v-2.418A1.75 1.75 0 0 0 12 12.25" clip-rule="evenodd"/>
+                        </svg>
                     </div>
                     
                     <!-- Texto -->
@@ -68,7 +87,9 @@
                 <div class="flex items-center justify-center mb-8">
                     <!-- Ícone -->
                     <div class="flex-shrink-0">
-                        <img src="{{ asset('images/icons/ficheiro.png') }}" alt="Ícone" class="w-12 h-12"> 
+                        <svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 2048 2048">
+                            <path fill="#00b0eb" d="M1920 512v1408H768v-256H512v-256H256V0h731l256 256h421v256zm-896-128h165l-165-165zm256 896V512H896V128H384v1152zm256 256V384h-128v1024H640v128zm257-896h-129v1024H896v128h897z"/>
+                        </svg>
                     </div>
                     
                     <!-- Texto -->
@@ -94,7 +115,7 @@
                     <!-- Círculo do número -->
                     <div class="flex justify-center items-center bg-sky-400 text-white font-bold w-10 h-10 rounded-full text-center">1</div>
                     <!-- Retângulo com a frase -->
-                    <div class="bg-sky-400 text-white font-bold px-3 py-2 rounded-lg w-full text-left">Fazer o Registro no Sistema</div>
+                    <div class="bg-sky-400 text-white font-bold px-3 py-2 rounded-lg w-full text-left">Autenticar no Sistema</div>
                 </div>
 
                 <!-- Passo 2 -->
