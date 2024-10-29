8120abc5b53f44e7c66abf4e97013b8b6fa90ea5
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2024-10-29 13:11:28 +0000

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,5 +5,5 @@
 @section('page-name', 'Home Dashboard')
 
 @section('content')
-    <h1 class="mt-10">Hello World!</h1>
+    <h1 class="mt-10 font-extrabold text-sky-400">Hello World!</h1>
 @endsection
