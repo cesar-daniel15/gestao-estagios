2c98ef970d5713546445482479beaaab812c18a9
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2024-11-05 19:45:39 +0000

    Create CourseResource.php

---
+++ app/Http/Resources/CourseResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CourseResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'acronym' => $this->acronym,
+            'institution' => [
+                'id' => $this->institution->id,
+                'name' => $this->institution->name,
+                'acronym' => $this->institution->acronym,
+            ],
+            'created_at' => $this->created_at->diffForHumans(),
+            'updated_at' => $this->updated_at->diffForHumans(),
+        ];
+    }
+}
