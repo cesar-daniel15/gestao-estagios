a0c52454321733522a0bd8eda43ef5934d888062
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2024-11-07 23:53:22 +0000

    Update institutions.blade.php

--- resources/views/admin/institutions.blade.php
+++ resources/views/admin/institutions.blade.php
@@ -48,10 +48,17 @@
                                 <td class="p-4 text-gray-600">{{ $institution['account_is_verified'] }}</td>
                                 <td class="p-4 text-gray-600">
                                     <div class="flex space-x-2">
-                                        <!-- Botão Editar -->
+                                        <!-- Botão Ver -->
                                         <a href="{{ route('institutions.show', $institution['id']) }}" class="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-2 rounded flex items-center">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 h-5">
                                                 <path fill="currentColor" d="M11 17h2v-6h-2zm1-8q.425 0 .713-.288T13 8t-.288-.712T12 7t-.712.288T11 8t.288.713T12 9m0 13q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8"/>
+                                            </svg>
+                                        </a>
+
+                                        <!-- Botão Editar -->
+                                        <a href="{{ route('institutions.update', $institution['id']) }}" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-2 rounded flex items-center">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 h-5">
+                                                <path fill="currentColor" d="m12.9 6.855l4.242 4.242l-9.9 9.9H3v-4.243zm1.414-1.415l2.121-2.121a1 1 0 0 1 1.414 0l2.829 2.828a1 1 0 0 1 0 1.415l-2.122 2.121z"/>
                                             </svg>
                                         </a>
 
