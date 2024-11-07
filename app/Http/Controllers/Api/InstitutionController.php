c4a594fbe9824aff5eaff8df98023cbc3f950de8
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2024-11-07 23:53:18 +0000

    Update InstitutionController.php

--- app/Http/Controllers/Api/InstitutionController.php
+++ app/Http/Controllers/Api/InstitutionController.php
@@ -37,7 +37,7 @@ class InstitutionController extends Controller
      */
     public function store(Request $request)
     {
-        // Armazena um novo recurso na db
+        // Armazena uma nova instituicao na db
 
         // Validação dos dados recebidos
         $validator = Validator::make($request->all(), [
@@ -80,11 +80,11 @@ class InstitutionController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Institution $institution)
     {
-        // Retorna um recurso específico pelo ID
+        // Retorna uma insituicao específica 
 
-        return new InstitutionResource(Institution::where('id',$id)->first());
+        return new InstitutionResource($institution);
     }
 
     /**
@@ -98,16 +98,65 @@ class InstitutionController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Institution $institution)
     {
-        // Atualiza um recurso existente
+        // Atualiza uma instituicao existente
+        // Validação dos dados recebidos para atualização
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255|unique:institutions,name,' . $id, // Ignora a exclusividade do ID
+            'acronym' => 'required|string|max:10',
+            'email' => 'required|email|max:255|unique:institutions,email,' . $id, // Ignora a exclusividade do ID
+            'password' => 'nullable|string|min:8', // Permite que a password seja opcional
+            'phone' => 'required|string|max:11|unique:institutions,phone,' . $id, // Ignora a exclusividade do ID
+            'address' => 'required|string|max:255',
+            'website' => 'nullable|url|max:255',
+            'logo' => 'nullable|string|max:255',
+        ]);
+
+        // Verifica se a validation falhou
+        if ($validator->fails()) {
+            return $this->error('Validation failed', 422, $validator->errors());
+        }
+
+        // Verifica se a validacao falhou
+        $validated = $validator->validated();
+
+        // Faz o Update
+        $update = $institution->update([
+            'name' => $validated['name'],
+            'acronym' => $validated['acronym'],
+            'email' => $validated['email'],
+            'password' => bcrypt($validated['password']),
+            'phone' => $validated['phone'],
+            'address' => $validated['address'],
+            'website' => $validated['website'],
+            'logo' => $validated['logo'],
+        ]);
+
+        if($update){
+            return $this->response('Institution updated successfully', 200, new InstitutionResource($institution));
+        }
+        else{
+            return $this->response('Institution not updated', 400);
+        }
+
     }
 
     /**
      * Remove the specified resource from storage
      */
-    public function destroy(string $id)
+    public function destroy(Institution $institution)
     {
-        // Remove um recurso da db     
+        // Remove uma instituicao da db     
+
+        $deleted = $institution->delete();
+
+        // Verificar se foi apagada
+        if($deleted){
+            return $this->response('Institution deleted successfully', 200);
+        }else
+        {
+            return $this->response('Institution not deleted', 400);
+        }
     }
 }
