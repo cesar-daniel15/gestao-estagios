f2a478ddac191ec64685a4177c5a268f08a5a132
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2024-11-07 23:53:27 +0000

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,17 +13,17 @@ Route::get('/user', function (Request $request) {
 // Grupo das rotas de InstitutionController
 Route::prefix('institutions')->group(function () {
     Route::get('/', [InstitutionController::class, 'index'])->name('institutions.index');  // Listar todas as instituições
-    Route::get('{id}', [InstitutionController::class, 'show'])->name('institutions.show');  // Mostrar uma instituição específica
+    Route::get('{institution}', [InstitutionController::class, 'show'])->name('institutions.show');  // Mostrar uma instituição específica
     Route::post('/', [InstitutionController::class, 'store'])->name('institutions.store'); // Criar uma nova instituição
-    Route::put('{id}', [InstitutionController::class, 'update'])->name('institutions.update');  // Atualizar uma instituição existente
-    Route::delete('{id}', [InstitutionController::class, 'destroy'])->name('institutions.destroy');  // Apagar uma instituição
+    Route::put('{institution}', [InstitutionController::class, 'update'])->name('institutions.update');  // Atualizar uma instituição existente
+    Route::delete('{institution}', [InstitutionController::class, 'destroy'])->name('institutions.destroy');  // Apagar uma instituição
 });
 
 // Grupo das rotas de CourseController
 Route::prefix('courses')->group(function () {
     Route::get('/', [CourseController::class, 'index'])->name('courses.index');  // Listar todos os cursos 
-    Route::get('{id}', [CourseController::class, 'show'])->name('courses.show');  // Mostrar um curso específico
+    Route::get('{course}', [CourseController::class, 'show'])->name('courses.show');  // Mostrar um curso específico
     Route::post('/', [CourseController::class, 'store'])->name('courses.store'); // Criar um novo curso
-    Route::put('{id}', [CourseController::class, 'update'])->name('courses.update');  // Atualizar um curso existente
-    Route::delete('{id}', [CourseController::class, 'destroy'])->name('courses.destroy');  // Apagar um curso
+    Route::put('{course}', [CourseController::class, 'update'])->name('courses.update');  // Atualizar um curso existente
+    Route::delete('{course}', [CourseController::class, 'destroy'])->name('courses.destroy');  // Apagar um curso
 });
