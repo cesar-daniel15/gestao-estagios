3c291c7172e54b16bb2bf58756c55cefcd4ef3b3
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2024-11-09 18:58:18 +0000

    Create Search Input

--- resources/views/admin/institutions.blade.php
+++ resources/views/admin/institutions.blade.php
@@ -9,15 +9,30 @@
         <div class="text-lg font-bold text-gray-600 mb-6">
             Instituições Existentes
         </div>
-        <div class="flex justify-end my-5">
+        <div class="flex justify-between items-center my-5">
+            <!-- Barra de pesquisa -->
+            <form class="hidden sm:flex  w-auto">  
+                <label for="simple-search" class="sr-only">Procurar</label>
+                <div class="relative w-full">
+                    <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Procurar uma Instituição..." required />
+                </div>
+                <button type="submit" class="p-2.5 ms-2 text-sm font-medium text-white bg-sky-400 rounded-lg hover:bg-sky-500 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                    <span class="sr-only">Procurar</span>
+                </button>
+            </form>
+
             <!-- Botão para abrir o modal -->
-            <button onclick="openModal()" class="bg-green-500 hover:bg-green-600 text-white font-bold rounded-xl py-2 px-4 flex text-sm items-center">
+            <button onclick="openModal()" class="bg-green-500 hover:bg-green-600 text-white font-bold rounded-xl p-2.5 flex text-sm items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 h-5 mr-2" fill="white">
                     <path d="M12 4.75c.69 0 1.25.56 1.25 1.25v4.75H18a1.25 1.25 0 1 1 0 2.5h-4.75V18a1.25 1.25 0 1 1-2.5 0v-4.75H6a1.25 1.25 0 1 1 0-2.5h4.75V6c0-.69.56-1.25 1.25-1.25"/>
                 </svg>
                 Registrar Instituição
             </button>
         </div>
+
         <div class="overflow-x-auto">
             <table class="table-auto w-full border-collapse text-center text-sm overflow-hidden rounded-xl">
                 <thead>
