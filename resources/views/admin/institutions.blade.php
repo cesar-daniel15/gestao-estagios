d3ba6d25158bbb58ef75d9452d8b76fb4541d598
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2024-11-11 01:02:10 +0000

    Update institutions.blade.php

--- resources/views/admin/institutions.blade.php
+++ resources/views/admin/institutions.blade.php
@@ -2,19 +2,54 @@
 
 @section('title', 'Gestão Estágios | Admin Dashboard')
 
-@section('page-name', 'Instituições /')
+@section('page-name', 'Instituições / Instituições')
 
 @section('content')
+
+    <!-- Alertas -->
+    @if(session('success'))
+        <div class="flex items-center p-4 mb-4 text-green-800 border-t-4 border-green-300 bg-green-50 dark:text-green-400 dark:bg-gray-800 dark:border-green-800" role="alert">
+            <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+            </svg>
+            <div class="ms-3 text-sm font-medium">
+                {{ session('success') }}
+            </div>
+            <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-green-400 dark:hover:bg-gray-700" data-dismiss-target="#alert-border-3" aria-label="Close">
+                <span class="sr-only">Dispensar</span>
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                </svg>
+            </button>
+        </div>
+    @endif
+
+    @if(session('error'))
+        <div class="flex items-center p-4 mb-4 text-red-800 border-t-4 border-red-300 bg-red-50 dark:text-red-400 dark:bg-gray-800 dark:border-red-800" role="alert">
+            <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+            </svg>
+            <div class="ms-3 text-sm font-medium">
+                {{ session('error') }}
+            </div>
+            <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700" data-dismiss-target="#alert-border-2" aria-label="Close">
+                <span class="sr-only">Dispensar</span>
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                </svg>
+            </button>
+        </div>
+    @endif
+
     <div class="mt-10 bg-white drop-shadow-md rounded-xl p-10">
         <div class="text-lg font-bold text-gray-600 mb-6">
             Instituições Existentes
         </div>
         <div class="flex justify-between items-center my-5">
             <!-- Barra de pesquisa -->
-            <form class="hidden sm:flex  w-auto">  
-                <label for="simple-search" class="sr-only">Procurar</label>
-                <div class="relative w-full">
-                    <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Procurar uma Instituição..." required />
+            <form class="hidden sm:flex">
+                <div class="relative w-auto">
+                    <input type="text" id="search" class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Procurar Instituição" required />
                 </div>
                 <button type="submit" class="p-2.5 ms-2 text-sm font-medium text-white bg-sky-400 rounded-lg hover:bg-sky-500 focus:ring-4 focus:outline-none focus:ring-blue-300">
                     <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -24,8 +59,8 @@
                 </button>
             </form>
 
-            <!-- Botão para abrir o modal -->
-            <button onclick="openModal()" class="bg-green-500 hover:bg-green-600 text-white font-bold rounded-xl p-2.5 flex text-sm items-center">
+            <!-- Botão para criar Instituicao -->
+            <button onclick="openCreateModal()" class="bg-green-500 hover:bg-green-600 text-white font-bold rounded-xl p-2.5 flex text-sm items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 h-5 mr-2" fill="white">
                     <path d="M12 4.75c.69 0 1.25.56 1.25 1.25v4.75H18a1.25 1.25 0 1 1 0 2.5h-4.75V18a1.25 1.25 0 1 1-2.5 0v-4.75H6a1.25 1.25 0 1 1 0-2.5h4.75V6c0-.69.56-1.25 1.25-1.25"/>
                 </svg>
@@ -42,7 +77,6 @@
                         <th class="p-4 border-b text-gray-600">Acrónimo</th>
                         <th class="p-4 border-b text-gray-600">Email</th>
                         <th class="p-4 border-b text-gray-600">Contacto</th>
-                        <th class="p-4 border-b text-gray-600">Morada</th>
                         <th class="p-4 border-b text-gray-600">Website</th>
                         <th class="p-4 border-b text-gray-600">Conta Verificada</th>
                         <th class="p-4 border-b text-gray-600">Ações</th>
@@ -56,13 +90,12 @@
                                 <td class="p-4 text-gray-600">{{ $institution['acronym'] }}</td>
                                 <td class="p-4 text-gray-600">{{ $institution['email'] }}</td>
                                 <td class="p-4 text-gray-600">{{ $institution['phone'] }}</td>
-                                <td class="p-4 text-gray-600">{{ $institution['address'] }}</td>
                                 <td class="p-4 text-gray-600">
                                     <a href="{{ $institution['website'] }}" class="text-sky-400 hover:underline" target="_blank">{{ $institution['website'] }}</a>
                                 </td>
                                 <td class="p-4 text-gray-600">{{ $institution['account_is_verified'] }}</td>
                                 <td class="p-4 text-gray-600">
-                                    <div class="flex space-x-2">
+                                    <div class="flex space-x-2 justify-center">
                                         <!-- Botão Ver -->
                                         <a href="{{ route('institutions.show', $institution['id']) }}" class="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-2 rounded flex items-center">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 h-5">
@@ -78,10 +111,10 @@
                                         </a>
 
                                         <!-- Botão Apagar -->
-                                        <form action="{{ route('institutions.destroy', $institution['id']) }}" method="POST" onsubmit="return confirm('Tem certeza que deseja apagar esta instituição?');">
+                                        <form id="deleteForm{{ $institution['id'] }}" action="{{ route('institutions.destroy', $institution['id']) }}" method="POST">
                                             @csrf
                                             @method('DELETE')
-                                            <button type="submit" class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-2 rounded flex items-center">
+                                            <button type="button" onclick="openDeleteModal({{ $institution['id'] }})" class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-2 rounded flex items-center">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 h-5">
                                                     <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 7h16m-10 4v6m4-6v6M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3"/>
                                                 </svg>
@@ -97,11 +130,12 @@
         </div>
     </div>
 
-    <!-- Modal -->
-    <div class="fixed inset-0 items-center sm:h-screen justify-center z-50 bg-black bg-opacity-50 hidden text-sm">
+    <!-- Modal Criar Nova Instituicao -->
+    <div id="createModal" class="fixed inset-0 items-center sm:h-screen justify-center z-50 bg-black bg-opacity-50 hidden text-sm">
         <div class="bg-white rounded-lg shadow-lg p-6 w-11/12 md:w-1/3">
-            <h2 class="text-2sm font-bold text-gray-700 mb-4 text-center">Registrar Instituição</h2>
-            
+            <h2 class="text-xl font-bold text-gray-700 mb-4 text-center">Registrar Instituição</h2>
+
+            <!-- Forma -->
             <form action="{{ route('institutions.store') }}" method="POST">
                 @csrf
                 <div class="mb-4">
@@ -139,26 +173,110 @@
                     <input type="url" id="website" name="website" class="border border-gray-300 rounded-lg w-full p-1 xl:p-2">
                 </div>
 
-                <div class="flex justify-end mt-4">
-                    <button type="button" class="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-2 xl:px-4 rounded mr-2" onclick="closeModal()">Cancelar</button>
+                <div class="flex justify-end">
+                    <button type="button" class="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-2 xl:px-4 rounded mr-2" onclick="closeCreateModal()">Cancelar</button>
                     <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-2 xl:px-4 rounded">Registrar</button>
                 </div>
             </form>
         </div>
     </div>
 
+    <!-- Modal de Confirmação -->
+    <div id="deleteModal" class="fixed inset-0 bg-black bg-opacity-50 items-center justify-center z-50 hidden">
+        <div class="bg-white p-6 rounded-lg w-1/3">
+            <h2 class="text-md font-smibold py-10 text-center text-gray-500">Tem a certeza que deseja apagar a instituição?</h2>
+            <div class="flex justify-center space-x-5">
+                <button class="bg-gray-500 hover:bg-gray-600 text-white p-2.5 font-bold rounded" onclick="closeDeleteModal()">Cancelar</button>
+                <button id="confirmDeleteButton" class="bg-red-500 hover:bg-red-600 text-white p-2.5 font-bold rounded">Confirmar</button>
+                </div>
+        </div>
+    </div>
+
+    <!-- Modal de Visualizacao -->
+    <div id="viewModal" class="fixed inset-0 items-center bg-black bg-opacity-50 justify-center z-50 hidden">
+        <div class="bg-white p-6 rounded-lg">
+            <div class="modal-content">
+                <h2 class="text-xl font-bold text-gray-700 mb-4 text-center">{{ $institution['name'] }}</h2>
+                <div class="modal-body flex text-base">
+                    <!-- Coluna da imagem -->
+                    <div class="flex-shrink-0 w-1/3">
+                        <img src="{{ $institution['logo'] }}" alt="Logo" class="w-full h-52 object-cover">
+                    </div>
+                    
+                    <!-- Coluna com as informações -->
+                    <div class="ml-4 w-2/3">
+                        <p><strong>Acrónimo:</strong> {{ $institution['acronym'] }}</p>
+                        <p><strong>Email:</strong> {{ $institution['email'] }}</p>
+                        <p><strong>Contacto:</strong> {{ $institution['phone'] }}</p>
+                        <p><strong>Morada:</strong>  {{ $institution['address'] }}</p>
+                        <p><strong>Website:</strong> <a href="{{ $institution['website'] }}" target="_blank">{{ $institution['website'] }}</a></p>
+                        <p><strong>Conta Verificada:</strong> {{ $institution['account_is_verified'] }}</p>
+                        <p><strong>Último Login:</strong> {{ $institution['last_login'] }}</p>
+                        <p><strong>Data de Criação:</strong> {{ $institution['created_at'] }}</p>
+                    </div>
+                </div>
+                <div class="modal-footer flex justify-end">
+                    <button type="button" class="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-2 xl:px-4 rounded mr-2" onclick="closeViewModal()">Fechar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script>
-        // Abrir o modal
-        function openModal() {
-            const modal = document.querySelector('.fixed.inset-0');
-            modal.style.display = 'flex'; 
+         // Abrir Modal para ver uma Instituicao
+        function openViewModal() {
+            const viewModal = document.querySelector('#viewModal');
+
+            viewModal.style.display = 'flex';
+            viewModal.classList.remove('hidden'); 
         }
 
-        // Fechar o modal
-        function closeModal() {
-            const modal = document.querySelector('.fixed.inset-0');
-            modal.style.display = 'none'; 
+        // Fechar Modal para ver uma Instituicao
+        function closeViewModal() {
+            const viewModal = document.querySelector('#viewModal');
+            
+            viewModal.style.display = 'none'; 
         }
+
+        // Abrir Modal para criar nova Instituicao
+        function openCreateModal() {
+            const createModal = document.querySelector('#createModal');
+
+            createModal.style.display = 'flex';
+            createModal.classList.remove('hidden'); 
+        }
+
+        // Fechar Modal para criar nova Instituicao
+        function closeCreateModal() {
+            const createModal = document.querySelector('#createModal');
+            
+            createModal.style.display = 'none'; 
+        }
+
+        // Abrir Modal para apagar uma Instituicao
+        function openDeleteModal(id) {
+            const deleteModal = document.querySelector('#deleteModal');
+            const deleteForm = document.querySelector('#deleteForm' + id);
+            
+            // Exibe o modal
+            deleteModal.style.display = 'flex';
+            deleteModal.classList.remove('hidden'); 
+
+            // Seleciona o botão de confirmação
+            const confirmDeleteButton = deleteModal.querySelector('#confirmDeleteButton');
+            
+            confirmDeleteButton.onclick = function () {
+                deleteForm.submit(); // Submete o formulário 
+            };
+        }
+
+        // Fechar Modal para apagar uma Instituicao
+        function closeDeleteModal() {
+            const deleteModal = document.querySelector('#deleteModal');
+            
+            deleteModal.style.display = 'none'; 
+        }
+
     </script>
 
 @endsection
