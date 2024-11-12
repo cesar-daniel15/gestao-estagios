cd66555dadc30dbf29d99bcfa5f9d6e364b961f6
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2024-11-12 15:34:31 +0000

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
+        
         // Criar 2 instituicoes ficticias
         Institution::factory()->count(2)->create();
 
