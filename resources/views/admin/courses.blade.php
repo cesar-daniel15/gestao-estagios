e37c0c6888a3b13b73b305ed1db600ad1435285c
RubenBenedito <162583130+RubenBenedito@users.noreply.github.com>
2024-11-14 22:20:42 +0000

    boton registar courses

--- resources/views/admin/courses.blade.php
+++ resources/views/admin/courses.blade.php
@@ -5,24 +5,32 @@
 @section('page-name', 'Cursos /')
 
 @section('content')
-    <div class="mt-10 bg-white drop-shadow-md rounded-xl p-10">
-        <div class="text-lg font-bold text-gray-600 mb-6">
-            Cursos Existentes
-        </div>
-        <div class="my-5">
-            <!-- Barra de pesquisa -->
-            <form class="flex justify-center md:justify-start">
-                <div class="flex">
-                    <div class="relative w-full">
-                        <input type="text" id="search" class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block pl-10 w-full p-2.5 text-start" 
-                            placeholder="Procurar por Utilizador" oninput="searchUser()" />
-                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" class="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-500">
-                            <path fill="currentColor" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14"/>
-                        </svg>
-                    </div>
+            <div class="mt-10 bg-white drop-shadow-md rounded-xl p-10">
+                <div class="text-lg font-bold text-gray-600 mb-6">
+                    Cursos Existentes
                 </div>
-            </form>
+            <div class="flex flex-col gap-5 md:flex-row justify-between items-center my-5">
+
+            <!-- Barra de pesquisa -->
+            <div class="flex">
+                <div class="relative w-full">
+                    <input type="text" id="search" class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block pl-10 w-full p-2.5 text-start" 
+                        placeholder="Procurar por Curso" oninput="searchInstitution()" />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" class="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-500">
+                        <path fill="currentColor" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14"/>
+                    </svg>
+                </div>
+            </div>
+
+            <!-- Botão para criar Curso -->
+            <button onclick="openModal('createModal')" class="bg-green-500 hover:bg-green-600 text-white font-bold rounded-xl p-2.5 flex text-sm items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 h-5 mr-2" fill="white">
+                    <path d="M12 4.75c.69 0 1.25.56 1.25 1.25v4.75H18a1.25 1.25 0 1 1 0 2.5h-4.75V18a1.25 1.25 0 1 1-2.5 0v-4.75H6a1.25 1.25 0 1 1 0-2.5h4.75V6c0-.69.56-1.25 1.25-1.25"/>
+                </svg>
+                Registrar Curso
+            </button>
         </div>
+
         <div class="overflow-x-auto">
             <table class="table-auto w-full border-collapse text-center text-sm overflow-hidden rounded-xl" id="userTable">
                 <thead>
