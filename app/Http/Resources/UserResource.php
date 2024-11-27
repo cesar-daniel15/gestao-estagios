a247dfc733a4eaffc995864341a59f7c0a3e3cdc
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2024-11-27 17:55:26 +0000

    Update UserResource.php

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -4,9 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Carbon\Carbon; // Importar o Carbon para manipulação de datas
-use App\Models\Institution; // Para verificar a tabela institution
-
+use Carbon\Carbon;
 
 class UserResource extends JsonResource
 {
@@ -17,8 +15,6 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        // return parent::toArray($request);
-
         // Traduz o perfil
         $profileTranslations = [
             'Institution' => 'Instituição',
@@ -28,44 +24,49 @@ class UserResource extends JsonResource
             'Admin' => 'Administrador',
         ];
 
+        // Informações adicionais baseadas no perfil do usuário
+        $associated_info = '';
+
+        if ($this->profile == 'Institution' && $this->institution) {
+            $associated_info = json_encode([
+                'id' => $this->institution->id,
+                'acronym' => $this->institution->acronym,
+                'phone' => $this->institution->phone,
+                'address' => $this->institution->address,
+                'logo' => $this->institution->logo,
+                'website' => $this->institution->website,
+            ]);
+        } elseif ($this->profile == 'Company' && $this->company) {
+            $associated_info = json_encode([
+                'id' => $this->company->id,
+                'phone' => $this->company->phone,
+                'industry' => $this->company->industry,
+                'brief_description' => $this->company->brief_description,
+                'address' => $this->company->address,
+                'foundation_date' => $this->company->foundation_date,
+                'logo' => $this->company->logo,
+            ]);
+        } elseif ($this->profile == 'Responsible' && $this->responsible) {
+            $associated_info = json_encode([
+                'id' => $this->responsible->id,
+                'phone' => $this->responsible->phone,
+                'picture' => $this->responsible->picture,
+            ]);
+        } elseif ($this->profile == 'Student' && $this->student) {
+            $associated_info = json_encode([
+                'id' => $this->student->id,
+                'assigned_internship_id' => $this->student->assigned_internship_id,
+                'phone' => $this->student->phone,
+                'picture' => $this->student->picture,
+            ]);
+        }
+
         return [
             'id' => $this->id,
             'name' => $this->name,
             'profile' => $profileTranslations[$this->profile] ?? $this->profile,
             'email' => $this->email,
-
-            'id_institution' => $this->institution ? [
-                'id' => $this->institution->id,
-                'acronym' => $this->institution->acronym, 
-                'phone' => $this->institution->phone, 
-                'address' => $this->institution->address, 
-                'logo' => $this->institution->logo, 
-                'website' => $this->institution->website, 
-            ] : null,
-
-            'id_company' => $this->company ? [
-                'id' => $this->company->id,
-                'phone' => $this->company->phone,
-                'industry' => $this->company->industry, 
-                'brief_description' => $this->company->brief_description, 
-                'address' => $this->company->address, 
-                'foundation_date' => $this->company->foundation_date, 
-                'logo' => $this->company->logo, 
-            ] : null,
-
-            'id_responsible' => $this->responsible ? [
-                'id' => $this->responsible->id,
-                'phone' => $this->responsible->phone,
-                'picture' => $this->responsible->picture, 
-            ] : null,
-
-            'id_student' => $this->student ? [
-                'id' => $this->student->id,
-                'assigned_internship_id' => $this->student->assigned_internship_id,
-                'phone' => $this->student->phone,
-                'picture' => $this->student->picture,
-            ] : null,
-
+            'associated_info' => $associated_info,  // Informações adicionais
             'token' => $this->token,
             'account_is_verified' => $this->account_is_verified ? 'Sim' : 'Não',
             'last_login' => $this->last_login ? Carbon::parse($this->last_login)->locale('pt')->diffForHumans() : 'Nunca',
