617990b0f24ffd822754cce4a131efffb4c71d8a
RubenBenedito <162583130+RubenBenedito@users.noreply.github.com>
2024-12-05 21:14:17 +0000

    Update units-curriculars.blade.php

--- resources/views/admin/units-curriculars.blade.php
+++ resources/views/admin/units-curriculars.blade.php
@@ -16,7 +16,7 @@
             
         <!-- Barra de pesquisa -->
         <div class="relative w-full md:w-auto mb-4 md:mb-0">
-            <input type="text" id="search" class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block pl-10 w-full p-2.5 text-start" placeholder="Procurar por Curso" oninput="searchCourse()" />
+            <input type="text" id="search" class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block pl-10 w-full p-2.5 text-start" placeholder="Procurar por Unidade Curricular" oninput="searchCourse()" />
             <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" class="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-500">
                 <path fill="currentColor" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14"/>
             </svg>
