ee81ad2b4def4799817d5d9077a7e56f3a3e0949
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2024-12-24 00:31:59 +0000

    Update CheckProfileCompletion.php

--- app/Http/Middleware/CheckProfileCompletion.php
+++ app/Http/Middleware/CheckProfileCompletion.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Closure;
@@ -9,39 +8,42 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CheckProfileCompletion
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
         // Verifica se o usuário está autenticado
         if (Auth::check()) {
             $user = Auth::user();
 
+            // Verifica se o perfil é 'Company' e se id_company é nulo
             if ($user->profile === 'Company' && is_null($user->id_company)) {
-                if (!$request->routeIs('company.profile')) {
-                    return redirect()->route('company.profile')->with('info', 'Por favor, atualize o seu perfil!');
+                if ($request->isMethod('post') || $request->routeIs('company.profile')) {
+                    return $next($request);
                 }
+                return redirect()->route('company.profile')->with('info', 'Por favor, atualize o seu perfil!');
             }
 
+            // Verifica se o perfil é 'Student' e se id_student é nulo
             if ($user->profile === 'Student' && is_null($user->id_student)) {
-                if (!$request->routeIs('student.profile')) {
-                    return redirect()->route('student.profile')->with('info', 'Por favor, atualize o seu perfil!');
+                if ($request->isMethod('post') || $request->routeIs('student.profile')) {
+                    return $next($request);
                 }
+                return redirect()->route('student.profile')->with('info', 'Por favor, atualize o seu perfil!');
             }
 
+            // Verifica se o perfil é 'Responsible' e se id_responsible é nulo
             if ($user->profile === 'Responsible' && is_null($user->id_responsible)) {
-                if (!$request->routeIs('responsible.profile')) {
-                    return redirect()->route('responsible.profile')->with('info', 'Por favor, atualize o seu perfil!');
+                if ($request->isMethod('post') || $request->routeIs('responsible.profile')) {
+                    return $next($request);
                 }
+                return redirect()->route('responsible.profile')->with('info', 'Por favor, atualize o seu perfil!');
             }
 
+            // Verifica se o perfil é 'Institution' e se id_institution é nulo
             if ($user->profile === 'Institution' && is_null($user->id_institution)) {
-                if (!$request->routeIs('institution.profile')) {
-                    return redirect()->route('institution.profile')->with('info', 'Por favor, atualize o seu perfil!');
+                if ($request->isMethod('post') || $request->routeIs('institution.profile')) {
+                    return $next($request);
                 }
+                return redirect()->route('institution.profile')->with('info', 'Por favor, atualize o seu perfil!');
             }
         }
 
