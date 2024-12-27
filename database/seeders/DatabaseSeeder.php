e92fe58cd3f551cbf5931363d8a91d848f9a2fcf
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2024-12-27 20:51:48 +0000

    Closes #4:  Make all Seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,12 @@
 
 namespace Database\Seeders;
 
-use App\Models\User; // Import a Model so Users
-use App\Models\Institution; // Import da Model das Institution
-use App\Models\Course; // Import da Model dos Courses
+use App\Models\User; 
+use App\Models\Institution; 
+use App\Models\Course; 
 use App\Models\Company; 
 use App\Models\Student; 
-use App\Models\UnitCurricular; // Import da Model das Unidades Curriculares
+use App\Models\UnitCurricular; 
 use App\Models\UcResponsible; 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -21,12 +21,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
+        // Criar Admin
         $admin = User::factory()->admin()->create();
 
         // Criar instituições
         $estg = Institution::factory()->estg()->create();
         $ese = Institution::factory()->ese()->create();
 
+        // Criar users da instituicao
         $institutionESTG = User::factory()->institutionESTG()->create();
         $institutionESE = User::factory()->institutionESE()->create();
 
@@ -43,14 +45,18 @@ class DatabaseSeeder extends Seeder
         // Criar Responsaveis de UC
         $tiago = UcResponsible::factory()->tiago()->create();
 
+        // Criar alunos
         $cesar = Student::factory()->cesar()->create();
         $ruben = Student::factory()->ruben()->create();
 
-        $cesarUser   = User::factory()->studentCesar()->create();
-        $rubenUser   = User::factory()->studentRuben()->create();
+        // Criar users dos alunos
+        $cesarUser = User::factory()->studentCesar()->create();
+        $rubenUser = User::factory()->studentRuben()->create();
 
+        // Criar user do responsavel
         $responsibleTiago = User::factory()->responsibleTiago()->create();
 
+        // Criar users das empresas
         $companyBrandit = User::factory()->companyBrandit()->create();
         $companyDigiheart = User::factory()->companyDigiheart()->create();
     }
