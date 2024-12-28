1163fca05e33b2b35618d8b719fcd6beef951f87
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2024-12-28 21:07:35 +0000

    Update Student Features

--- resources/views/admin/students.blade.php
+++ resources/views/admin/students.blade.php
@@ -118,12 +118,12 @@
                                     {{ $student['ucs']->first()['course']['name'] }} 
                                 @endif
                             </td>
-                            <td class="p-4 text-gray-600">{{ $student['assigned_internship_id']}}</td>
+                            <td class="p-4 text-gray-600">{{ $student['assigned_internship_id'] ? $student['internship_offer']['title'] : 'Sem Estágio' }}</td>
                             <td class="p-4 text-gray-600">
                                 <div class="flex space-x-2 justify-center">
                                         
                                     <!-- Botão Ver -->
-                                    <a onclick="viewModal({{ $student['id'] }}, '{{($student['user']['name']) }}', '{{ $student['phone'] }}', '{{ asset($student['picture']) }}', '{{ $student['assigned_internship_id'] }}', '{{ $student['created_at'] }}')" class="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-2 rounded flex items-center">                                        
+                                     <a onclick="viewModal({{ $student['id'] }}, '{{ $student['user']['name'] }}', '{{ $student['phone'] }}', '{{ asset($student['picture']) }}', '{{ $student['assigned_internship_id'] ? $student['internship_offer']['title'] : 'Sem Estágio' }}', '{{ $student['created_at'] }}')" class="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-2 rounded flex items-center">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 h-5">
                                             <path fill="currentColor" d="M11 17h2v-6h-2zm1-8q.425 0 .713-.288T13 8t-.288-.712T12 7t-.712.288T11 8t.288.713T12 9m0 13q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8"/>
                                         </svg>
