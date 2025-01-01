c009097aee9c5887187ff7cd552c546cc8a46efb
RubenBenedito <162583130+RubenBenedito@users.noreply.github.com>
2025-01-01 17:41:40 +0000

    Update sidebar.blade.php

--- resources/views/users/responsible/layouts/components/sidebar.blade.php
+++ resources/views/users/responsible/layouts/components/sidebar.blade.php
@@ -64,7 +64,7 @@
 
             <!-- Notificação -->
             <li>
-               <a href="{{ url('/responsible/notify') }}" class="flex items-center p-2 text-gray-500 hover:text-white rounded-lg hover:bg-sky-400 group">
+               <a href="{{ url('/responsible/notifications') }}" class="flex items-center p-2 text-gray-500 hover:text-white rounded-lg hover:bg-sky-400 group">
                <svg xmlns="http://www.w3.org/2000/svg" width="1.6em" height="1.6em" viewBox="0 0 24 24" class="ms-2"><g fill="none" stroke="currentColor" stroke-width="2">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M6 19v-9a6 6 0 0 1 6-6v0a6 6 0 0 1 6 6v9M6 19h12M6 19H4m14 0h2m-9 3h2"/><circle cx="12" cy="3" r="1"/></g>
                </svg>
