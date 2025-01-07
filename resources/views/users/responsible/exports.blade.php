acb0d07d0e61d977077d35d3dd822c6b0e7d0b61
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2025-01-07 03:01:41 +0000

    Update exports.blade.php

--- resources/views/users/responsible/exports.blade.php
+++ resources/views/users/responsible/exports.blade.php
@@ -18,7 +18,7 @@
 
             <!-- Barra de pesquisa -->
             <div class="relative w-full md:w-auto mb-4 md:mb-0">
-                <input type="text" id="search" class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block pl-10 w-full p-2 text-start" placeholder="Procurar por Título" oninput="search()" />
+                <input type="text" id="search" class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block pl-10 w-full p-2 text-start" placeholder="Procurar por Aluno" oninput="search()" />
                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" class="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-500">
                     <path fill="currentColor" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14"/>
                 </svg>
@@ -55,7 +55,7 @@
             <table class="table-auto w-full border-collapse text-center text-sm overflow-hidden rounded-xl hidden" id="table">
                 <thead>
                     <tr class="bg-gray-100">
-                        <th class="p-4 border-b text-gray-600">Nome do Aluno</th>
+                        <th class="p-4 border-b text-gray-600 ">Nome do Aluno</th>
                         <th class="p-4 border-b text-gray-600">Registos Diários</th>
                         <th class="p-4 border-b text-gray-600">Relatório Final</th>
                         <th></th>
@@ -70,7 +70,7 @@
                     @else
                         @foreach($students as $student)
                             <tr class="border-b hover:bg-gray-50">
-                                <td class="p-4 text-gray-600 student-title">
+                                <td class="p-4 text-gray-600 student-name">
                                         {{ $student['user']['name'] }}
 
                                 </td>
@@ -134,9 +134,9 @@
             const rows = document.querySelectorAll("#table tbody tr"); 
 
             rows.forEach(row => {
-                const notificationTitle = row.querySelector(".notification-title").textContent.toLowerCase();
+                const name = row.querySelector(".student-name").textContent.toLowerCase();
                 
-                row.style.display = notificationTitle.includes(searchValue) ? "" : "none";
+                row.style.display = name.includes(searchValue) ? "" : "none";
             });
         }
 
