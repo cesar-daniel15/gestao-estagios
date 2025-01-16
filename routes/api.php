17d073a47020475a5dd020684f1aa1d799580441
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2025-01-16 23:31:42 +0000

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\UserController;
-use App\Http\Resources\UserResource;
-use App\Models\User;
+use App\Http\Controllers\Api\UserController;
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-});
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::prefix('users')->group(function () {
-    Route::get('/', [UserController::class, 'index'])->name('users.index');
-    Route::get('{user}', [UserController::class, 'show'])->name('users.show');
-    Route::post('/', [UserController::class, 'store'])->name('users.store');
-    Route::put('{user}', [UserController::class, 'update'])->name('users.update');
-    Route::delete('{user}', [UserController::class, 'destroy'])->name('users.destroy');
-});
+    Route::get('/users', [UserController::class, 'index']); 
+    Route::get('/users/{user}', [UserController::class, 'show']); 
+    Route::post('/users', [UserController::class, 'store']); 
+    Route::put('/users/{user}', [UserController::class, 'update']);
+    Route::delete('/users/{user}', [UserController::class, 'destroy']);
